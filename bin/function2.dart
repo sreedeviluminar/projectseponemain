d58db484f23d93dcd2b30da1f734c3bb33b014bc
Sreedevi <sreedevi@luminartechnolab.com>
2022-09-21 13:38:52 +0530

    commit

--- bin/function2.dart
+++ bin/function2.dart
@@ -1,6 +1,11 @@
 void add(int a, int b){ // here a and b are parameters /arguments / formal parameters
   print('sum = ${a+b}');
 }
+
+showmydetails(String name, int age , String email, String Qualification, int phone){
+
+}
+
 void main(){
   add(43,56);
   add(40, 20);
