29155ff0560599ad1311025a84f0a6ef1b6a0786
Sreedevi <sreedevi@luminartechnolab.com>
2022-09-26 14:02:02 +0530

    commit

--- bin/forloop.dart
+++ bin/forloop.dart
@@ -2,7 +2,9 @@
 void main(){
   //print 1 to 10
   for(int i = 1 ; i <= 10 ; i++){
-    print(i);
+    if(i % 2 != 0) {
+        print(i);
+    }
   }
 }
 /* i = 1 1<=10  true print 1  i++ = 2
---
+++ bin/sumof10naturalnumbers.dart
@@ -0,0 +1,8 @@
+void main(){
+  int sum = 0; // for storing sum in each iteration
+  for(int i = 1 ; i <= 10 ; i++){
+    sum = sum+i;
+  }
+  print (sum);  // print the value of sum after the execution of loop
+
+}
